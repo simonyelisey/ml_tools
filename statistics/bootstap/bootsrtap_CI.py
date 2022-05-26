4f5e5b6ac5ffe2ca792132c2ef37af7c5d526b70
Simon Yelisey <65309131+simonyelisey@users.noreply.github.com>
2022-05-26 10:46:13 +0300

    Create bootsrtap_CI.py
    добавил файл с функциями для реализации доверительного интервала методом Бутстрэп

---
+++ statistics/bootstap/bootsrtap_CI.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def bootstrap_samples(data: np.array, n_samples: int):
+    """
+    Функция генерирует новые выборки методом Bootstrap
+
+    :param data: np.array - генеральная выборка, на основе котораой генерируются выборки
+    :param n_samples: int-  количество генерируемых выборок
+    :return: np.array размера n_samples x len(data) - каждая строчка сгенерированная выборка
+    """
+    # генерация случайных индексов от 0 до len(data)
+    idxs = np.random.randint(0, len(data), size=(n_samples, len(data)))
+    # создание выборкок на основе сгенерированных индексов
+    bootstrapsamples = data[idxs]
+
+    return bootstrapsamples
+
+
+def confident_interval(stats_list: np.array, alpha: float):
+    """
+    Функция создает доверительный интервал статистик, полученных из сгенерированных выборок методом Bootstrap
+
+    :param stats_list: np.array - список статистик, посчитанных на всех выборках
+    :param alpha: float - уровень значимости
+    :return: list с нижней и верхней границей доверительного интервала
+    """
+    low_boundary = np.percentile(stats_list, 100 * alpha / 2)
+    high_boundary = np.percentile(stats_list, 100 * (1 - alpha / 2))
+
+    return [low_boundary, high_boundary]
+  
