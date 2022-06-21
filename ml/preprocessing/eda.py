aa21b3cb4937f17c53702de73bb2d9b21f9b255c
Simon Yelisey <yeliseevsemyon@gmail.com>
2022-06-21 12:42:12 +0300

    Update eda.py

--- ml/preprocessing/eda.py
+++ ml/preprocessing/eda.py
@@ -7,18 +7,18 @@ def non_informative_features(dataframe: pd.DataFrame, threshold: float) -> list:
   return: list of non-informative features
   
   """
-    low_inf_feat = []
-    
-    for col in dataframe.columns:
-        vc = dataframe[col].value_counts()
-        first_value_proportion = vc.values[0] / dataframe.shape[0]
-        
-        if first_value_proportion >= threshold:
-            low_inf_feat.append(col)
-            
-    if len(low_inf_feat) == 0:
-        print('There is NO low informative features in dataframe.')
-    else:
-        print(f'There is(are) {len(low_inf_feat)} low informative feature(s).')
-        
-        return low_inf_feat
+  low_inf_feat = []
+
+  for col in dataframe.columns:
+      vc = dataframe[col].value_counts()
+      first_value_proportion = vc.values[0] / dataframe.shape[0]
+
+      if first_value_proportion >= threshold:
+          low_inf_feat.append(col)
+
+  if len(low_inf_feat) == 0:
+      print('There is NO low informative features in dataframe.')
+  else:
+      print(f'There is(are) {len(low_inf_feat)} low informative feature(s).')
+
+      return low_inf_feat
