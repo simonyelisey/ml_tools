bf98c85213351c3aa5453b14015c85cef17fe93b
Simon Yelisey <yeliseevsemyon@gmail.com>
2022-08-18 14:16:41 +0300

    Update time_series_preprocessing.py

--- ml/preprocessing/time_series_preprocessing.py
+++ ml/preprocessing/time_series_preprocessing.py
@@ -32,4 +32,33 @@ def treat_outliers_if(data: pd.DataFrame, target: str, date_col: str, test_dates
     whole_data.loc[data_to_treat.index.values, target] = data_to_treat[target]
 
     return whole_data
-  
+
+
+def treat_deficit(data: pd.DataFrame, deficit_col: str, target: str, date_col: str, test_dates: list):
+    """
+    The function treats the months with deficit:
+        - if sells == 0 and days of deficit >= 15, then replace the value with lag(1).expanding().median()
+
+    :param data: pd.DataFrame with next cols: SKU, mnth, target
+    :param deficit_col: str name of col with amount of days of deficit
+    :param target: str name of target variable treat to
+    :param date_col: str name of date column
+    :param test_dates: list of test part dates don't treat to
+    :return: pd.DataFrame treated values with
+    """
+    # initialize the data
+    whole_data = data.copy()
+    whole_data.reset_index(drop=True, inplace=True)
+    data_to_treat = whole_data[whole_data[date_col] < test_dates[0]]
+    # create expanding median feature
+    data_to_treat['expand_median'] = data_to_treat[target].shift(1).expanding().median()
+    # treat the deficit
+    data_to_treat.loc[(data_to_treat[target] == 0) &
+                      (data_to_treat[deficit_col] >= 15), target] = data_to_treat['expand_median']
+    data_to_treat.drop('expand_median', axis=1, inplace=True)
+    # replace original values with treated values
+    whole_data.iloc[data_to_treat.index.values, :] = data_to_treat
+    # in case if deficit was at first position
+    whole_data[target] = whole_data[target].bfill()
+
+    return whole_data
